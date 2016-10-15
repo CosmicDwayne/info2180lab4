a28cfdd1358232483fa02f7f60c106513309cec1
CosmicDwayne <dhs.harris@gmail.com>
2016-10-14 23:57:51 -0500

    lk

--- maze.js
+++ maze.js
@@ -1,19 +1,45 @@
 
-
-
-window.onload = function() {
-	
-	document.getElementById('boundary1').addEventListener("mouseover",allborders);
-	
+function change(pro){
+	gamel=true;
+	for (var i=0; i<=pro.length;i++){
+	pro[i].setAttribute("class","boundary");
 	}
+}
+function endgame(){
+ if(gamel){
+	 alert("You win!");
+	 }else{
+	 alert("You Lose! Try Again....");
+	 }
+
+}
+
+
+
+/*function allborders(){
 	
-function allborders(){
-	hit=true
-	var walls=document.querySelectorAll(".boundary");
-	for (var i=0;i<walls.length;i++)
+	var borders=document.querySelectorAll(".boundary");
+	
+	for (i=0;i< borders.length;i++){
+		borders[i].className= "boundary youlose";
+		}
+	}
+
+function end(){
+	var ending = document.getElementById("end") 
+	ending.addEventListener("mouseover",hitEnd);
+	
+	boundary.className="boundary youlose";
 	{
-		walls[i].className="boundary youlose";
-	document.getElementById("status").innerHTML = "You Loser";
+		var hitE = true;
 	}
-	}
+}
 
+function hitEnd(){
+	if (hitE = true)
+	{
+		alert("You Lose! Try Again....");
+	}else{
+		alert("You win!");
+	}
+}*/
